e88af5ecfc10896ccd940fcf45b4c1e04ce6693a
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-07-04 19:57:03 -0400

    Added reference to custom network packet api

--- project/SamSWAT.HeliCrash/Plugin.cs
+++ project/SamSWAT.HeliCrash/Plugin.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using NetworkPacketAPI;
 using StayInTarkov;
 
 namespace SamSWAT.HeliCrash.ArysReloaded
@@ -36,17 +37,7 @@ namespace SamSWAT.HeliCrash.ArysReloaded
                 new AcceptableValueRange<int>(0, 100)));
             
             // Patch network packet in
-            
-            StayInTarkovHelperConstants.Logger.LogInfo($"Trying to patch in {nameof(HeliCrashPacket)}");
-            var sit_types =
-                typeof(StayInTarkovHelperConstants).GetField("_sitTypes", BindingFlags.Static | BindingFlags.NonPublic);
-           
-            StayInTarkovHelperConstants.Logger.LogInfo($"Helicrash is patching in {nameof(HeliCrashPacket)}");
-            var new_types = new List<Type>();
-            new_types.Add(typeof(HeliCrashPacket));
-            var merged = StayInTarkovHelperConstants.SITTypes.Union(new_types).ToArray();
-            sit_types.SetValue(null, merged);
-            StayInTarkovHelperConstants.Logger.LogInfo($"Helicrash is finished patching {nameof(HeliCrashPacket)}");
+            PacketInjector.Inject<HeliCrashPacket>();
         }
     }
 }
